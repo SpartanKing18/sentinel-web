78cd221ce67e487d840dcf38e477600fe696a905
SpartanKing18 <jawahar6k@gmail.com>
2026-08-08 13:02:09 -0700

    Release v2.20.0: downloads -> 2.20.0

--- public/js/landing.js
+++ public/js/landing.js
@@ -4,9 +4,9 @@
 // Installers are hosted on GitHub Releases (too large to commit to the repo).
 const REL = "https://github.com/SpartanKing18/sentinel-web/releases/download/sentinel";
 const APP_FILES = {
-  linux: REL + "/sentinel-app_2.19.0_amd64.deb",
-  appimage: REL + "/Sentinel-2.19.0.AppImage",
-  windows: REL + "/Sentinel.Setup.2.19.0.exe",
+  linux: REL + "/sentinel-app_2.20.0_amd64.deb",
+  appimage: REL + "/Sentinel-2.20.0.AppImage",
+  windows: REL + "/Sentinel.Setup.2.20.0.exe",
 };
 const SITE = "https://sentinel-web-2hq9.onrender.com";
 const EDITIONS = [
